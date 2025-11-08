f1ca0a93e13df7d0711a72a5ad6b0c076a083861
alejandrocqvd <alejandrocqvd@gmail.com>
2025-11-08 13:57:53 -0700

    jericho

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import Account from '@/components/Account'
+import Auth from '@/components/Auth'
+import '@/globals.css'
+import { supabase } from '@/lib/supabase'
+import { Session } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
+import { View } from 'react-native'
+
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  return (
+    <View>
+      {session && session.user ? <Account key={session.user.id} session={session} /> : <Auth />}
+    </View>
+  )
+}
