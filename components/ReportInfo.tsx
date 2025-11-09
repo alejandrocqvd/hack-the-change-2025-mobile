4ee0b6de730f60b40a09b4391f049e21f38e6a05
alejandrocqvd <alejandrocqvd@gmail.com>
2025-11-08 22:54:12 -0700

    notifs

--- components/ReportInfo.tsx
+++ components/ReportInfo.tsx
@@ -62,7 +62,6 @@ export default function ReportInfoPage() {
         </View>
       </ScrollView>
 
-      {/* Confirmation Popup */}
       <ConfirmModal
         visible={showConfirm}
         title="Delete Report"
